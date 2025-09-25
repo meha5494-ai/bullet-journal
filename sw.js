7dac84af81d123ecec222f4dd6be3e20cf7ab513
meha5494-ai <meha5494@gmail.com>
2025-09-25 19:09:44 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'bujo-cache-v1';
+const ASSETS = ['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  e.respondWith(
+    caches.match(req).then(res => res || fetch(req).then(net => {
+      if (req.method==='GET' && net.ok) {
+        const copy = net.clone();
+        caches.open(CACHE).then(c => c.put(req, copy));
+      }
+      return net;
+    }).catch(()=>caches.match('./index.html')))
+  );
+});
